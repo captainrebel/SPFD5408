282e464b89549da71b68d6887f4a0f44bf50d2a0
captainrebel <javad1365ba@yahoo.com>
2016-11-26 14:53:18 -0800

    Update SPFD5408_Adafruit_TFTLCD.cpp

--- SPFD5408_Adafruit_TFTLCD.cpp
+++ SPFD5408_Adafruit_TFTLCD.cpp
@@ -29,11 +29,11 @@
 
 #include "pin_magic.h"
 
-//#define TFTWIDTH   320
-//#define TFTHEIGHT  480
+#define TFTWIDTH   320
+#define TFTHEIGHT  480
 
-#define TFTWIDTH   240
-#define TFTHEIGHT  320
+//#define TFTWIDTH   240
+//#define TFTHEIGHT  320
 
 // LCD controller chip identifiers
 #define ID_932X    0
